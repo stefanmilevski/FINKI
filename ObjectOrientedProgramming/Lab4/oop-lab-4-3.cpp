9544d1869e651d1d208f694de09bf1593de63e27
stefanm334 <stefan_m32@hotmail.com>
2022-05-19 07:07:35 +0200

    Update oop-lab-4-3.cpp

--- ObjectOrientedProgramming/Lab4/oop-lab-4-3.cpp
+++ ObjectOrientedProgramming/Lab4/oop-lab-4-3.cpp
@@ -140,11 +140,65 @@ public:
 
 
 
-int main()
-{
-    ParkingPlac p("aleksander","123",50);
-    p.pecati();
+int main(){
 
-    return 0;
+	ParkingPlac p[100];
+	int n,m;
+	char adresa[50],id[50];
+	int brojcasovi,cenacas;
+	cin>>n;
+	if(n > 0){
 
+
+		for (int i=0;i<n;i++){
+	        cin.get();
+			cin.getline(adresa,50);
+			cin>>id>>cenacas;
+
+			ParkingPlac edna(adresa,id,cenacas);
+
+	        p[i]=edna;
+		}
+
+		//plakjanje
+		cin>>m;
+		for (int i=0;i<m;i++){
+
+			cin>>id>>brojcasovi;
+
+	        int findId=false;
+	        for (int j=0;j<m;j++){
+	            if (strcmp(p[j].getId(),id)==0){
+	                p[j].platiCasovi(brojcasovi);
+	                findId=true;
+	            }
+	        }
+			if (!findId)
+	        cout<<"Ne e platen parking. Greshen ID."<<endl;
+		}
+
+	    cout<<"========="<<endl;
+	    ParkingPlac pCentar("Cvetan Dimov","C10",80);
+		for (int i=0;i<n;i++)
+	        if (p[i].daliIstaAdresa(pCentar))
+	            p[i].pecati();
+	} else {
+
+		ParkingPlac najdobarPlac("Mars", "1337", 1);
+	    int brVozila;
+	    cin >> brVozila;
+	    for(int i = 0; i < brVozila; ++i){
+
+	    	char boja[20];
+	    	char brend[20];
+	    	char model[20];
+
+	    	cin >> boja >> brend >> model;
+	    	Avtomobil novAvtomobil(boja, brend, model);
+	    	najdobarPlac.parkirajVozilo(novAvtomobil);
+	    }
+	    if(brVozila != 0)
+	    najdobarPlac.pecatiParkiraniVozila();
+
+	}
 }
